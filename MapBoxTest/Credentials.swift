866e871354cfa4fad264d41e8d3efd2790fa94f6
David Brunow <helloDavid@brunow.org>
2016-06-11 17:40:31 -0500

    Initial Commit

---
+++ MapBoxTest/Credentials.swift
@@ -0,0 +1,14 @@
+//
+//  Credentials.swift
+//  MapBoxTest
+//
+//  Created by David Brunow on 6/11/16.
+//  Copyright © 2016 Shepherd Dog. All rights reserved.
+//
+
+import Foundation
+
+struct Credentials
+{
+    static let GMSServicesAPIKey = "INSERT GMS SERVICES API KEY HERE"
+}
--- MapBoxTest/ViewController.swift
+++ MapBoxTest/ViewController.swift
@@ -6,20 +6,64 @@
 //  Copyright © 2015 Shepherd Dog. All rights reserved.
 //
 
+import GoogleMaps
+import Mapbox
+import MapKit
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+    var appleMap:   MKMapView!
+    var mapBoxMap:  MGLMapView!
+    var googleMap:  GMSMapView!
+    
+    enum MapBrand
+    {
+        case Apple
+        case Google
+        case MapBox
+    }
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let mapBrand = MapBrand.Google
+        
+        switch mapBrand
+        {
+        case .Apple:
+            appleMap = MKMapView(frame: view.bounds)
+            appleMap.mapType = .Hybrid
+            
+            view.addSubview(appleMap)
+        case .Google:
+            let camera = GMSCameraPosition.cameraWithLatitude(-33.86,
+                longitude: 151.20, zoom: 6)
+            googleMap = GMSMapView.mapWithFrame(CGRectZero, camera: camera)
+            googleMap.myLocationEnabled = true
+            googleMap.mapType = kGMSTypeHybrid
+            
+            self.view = googleMap
+            
+            let marker = GMSMarker()
+            marker.position = CLLocationCoordinate2DMake(-33.86, 151.20)
+            marker.title = "Sydney"
+            marker.snippet = "Australia"
+            marker.map = googleMap
+        case .MapBox:
+            let styleURL = NSURL(string: "asset://styles/satellite-v8.json")
+            
+            mapBoxMap = MGLMapView(frame: view.bounds, styleURL: styleURL)
+            
+            view.addSubview(mapBoxMap)
+        }
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
